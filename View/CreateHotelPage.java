b4b359f3b30477fe7fcd16573e53e75e3fb38ad4
XENNNOOOO <161929971+XENNNOOOO@users.noreply.github.com>
2024-07-12 14:28:51 +0800

    Add files via upload

---
+++ View/CreateHotelPage.java
@@ -0,0 +1,129 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CreateHotelPage implements ActionListener{
+    Color darkGreen = new Color(0x006937);
+    ImageIcon bgImage = new ImageIcon("images/Background.png");
+    ImageIcon logoImage = new ImageIcon("images/Logo.png");
+
+    public void launchCreateHotelPage(JFrame mainFrame) {
+        //Panel for bgImage
+        JPanel backgroundPanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(bgImage.getImage(), 0, 0, getWidth(), getHeight(), this);
+            }
+        };
+        backgroundPanel.setPreferredSize(new Dimension(bgImage.getIconWidth(), bgImage.getIconHeight()));
+
+        //Layeredpane for texts and buttons
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setPreferredSize(new Dimension(bgImage.getIconWidth(), bgImage.getIconHeight()));
+
+        //Text Field
+        JLabel promptLbl = new JLabel();
+        promptLbl.setText("Hotel Name: ");
+        promptLbl.setForeground(Color.WHITE);
+        promptLbl.setFont(new Font("Helvetica", Font.BOLD, 15));
+        promptLbl.setBounds(180, -50, bgImage.getIconWidth(), bgImage.getIconHeight());
+
+        JTextField hotelNameField = new JTextField();
+        hotelNameField.setColumns(15);
+        hotelNameField.setBounds(270, 200, 210, 30);
+
+        //Buttons
+        RoundedButton subButton = new RoundedButton("Submit");
+        subButton.setFont(new Font("Helvetica", Font.PLAIN, 14));
+        subButton.setForeground(Color.WHITE);
+        subButton.setBackground(darkGreen);
+        subButton.setBounds(375, 260, 100, 35); 
+        subButton.setOpaque(true); 
+        subButton.setBorder(null);
+        subButton.setContentAreaFilled(true); 
+        subButton.setCustomBorderColor(Color.WHITE); 
+        subButton.setCustomBorderThickness(2);
+
+        RoundedButton exButton = new RoundedButton("Exit");
+        exButton.setFont(new Font("Helvetica", Font.PLAIN, 14));
+        exButton.setForeground(Color.WHITE);
+        exButton.setBackground(Color.RED);
+        exButton.setBounds(185, 260, 100, 35); 
+        exButton.setOpaque(true); 
+        exButton.setBorder(null);
+        exButton.setContentAreaFilled(true); 
+        exButton.setCustomBorderColor(Color.WHITE); 
+        exButton.setCustomBorderThickness(2);
+
+
+        //Buttons ActionListener
+        subButton.addActionListener(e -> System.out.println("Submit button clicked!"));
+        exButton.addActionListener(e -> {
+            mainFrame.dispose();
+            LaunchPage lp = new LaunchPage();
+            lp.launchLaunchPage();
+        });
+        
+        //Label for Menu label
+        JLabel menuLabel = new JLabel("HOTEL CREATOR");
+        menuLabel.setHorizontalTextPosition(JLabel.RIGHT);
+        menuLabel.setVerticalTextPosition(JLabel.TOP);
+        menuLabel.setForeground(Color.WHITE);
+        menuLabel.setFont(new Font("Helvetica", Font.BOLD, 32));
+        menuLabel.setBounds(200, -115, bgImage.getIconWidth(), bgImage.getIconHeight());
+
+        //Layers
+        layeredPane.add(menuLabel, JLayeredPane.PALETTE_LAYER); 
+        layeredPane.add(promptLbl, JLayeredPane.POPUP_LAYER); 
+        layeredPane.add(hotelNameField, JLayeredPane.POPUP_LAYER); 
+        layeredPane.add(subButton, JLayeredPane.POPUP_LAYER);
+        layeredPane.add(exButton, JLayeredPane.POPUP_LAYER);
+
+
+        //Add Conditions
+        layeredPane.add(showUpdateStatus(mainFrame, true), JLayeredPane.POPUP_LAYER);
+
+        //JFrame
+        mainFrame.setTitle("Hotel Reservation System");
+        mainFrame.setSize(660, 530);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setResizable(false);
+        mainFrame.setIconImage(logoImage.getImage());
+
+        //backgroundPanel and layeredPane on JFrame
+        mainFrame.setContentPane(backgroundPanel);
+        mainFrame.getContentPane().setLayout(new BorderLayout());
+        mainFrame.getContentPane().add(layeredPane, BorderLayout.CENTER);
+
+        mainFrame.setLocationRelativeTo(null); // Center JFrame on screen
+        mainFrame.setVisible(true);
+    }
+
+
+    public JLabel showUpdateStatus(JFrame mainFrame, boolean isValid) {
+        JLabel statusLbl;
+
+        if(isValid) {
+            statusLbl = new JLabel("Update Successful!");
+            statusLbl.setBounds(230, 200, bgImage.getIconWidth(), bgImage.getIconHeight());
+
+        }
+        else {
+            statusLbl = new JLabel("Invalid Name!");
+            statusLbl.setBounds(265, 200, bgImage.getIconWidth(), bgImage.getIconHeight());
+
+        }
+
+        statusLbl.setHorizontalTextPosition(JLabel.RIGHT);
+        statusLbl.setVerticalTextPosition(JLabel.TOP);
+        statusLbl.setForeground(Color.WHITE);
+        statusLbl.setFont(new Font("Helvetica", Font.BOLD, 25));
+        
+        return statusLbl;
+    }
+
+
+    
+}
---
+++ View/HighLvlInfoPage.java
@@ -0,0 +1,118 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.Border;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class HighLvlInfoPage {
+    //lorem is a placeholder, make sure to add a String hotelNames in the parameters
+    public String lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    Color darkGray = new Color(0x20, 0x24, 0x25);
+    Color darkBlue = new Color(0x15, 0x68, 0xB3);
+    Color lightBlue = new Color(0x15, 0x68, 0xB3);
+
+    
+    public void launchHighLvlInfoPage(JFrame mainFrame) {
+        //Images
+         ImageIcon bgImage = new ImageIcon("images/Background.png");
+         ImageIcon logoImage = new ImageIcon("images/Logo.png");
+ 
+         //Panel for bgImage
+         JPanel backgroundPanel = new JPanel() {
+             @Override
+             protected void paintComponent(Graphics g) {
+                 super.paintComponent(g);
+                 g.drawImage(bgImage.getImage(), 0, 0, getWidth(), getHeight(), this);
+             }
+         };
+         backgroundPanel.setPreferredSize(new Dimension(bgImage.getIconWidth(), bgImage.getIconHeight()));
+ 
+         //Layeredpane for texts and buttons
+         JLayeredPane layeredPane = new JLayeredPane();
+         layeredPane.setPreferredSize(new Dimension(bgImage.getIconWidth(), bgImage.getIconHeight()));
+
+         //Font for JTextArea
+         Font font = new Font("Helvetica", Font.PLAIN, 12);
+
+         //JTextArea (Hotel List)
+         JTextArea hotelList = new JTextArea(8, 27);
+         hotelList.setFont(font);
+         hotelList.setForeground(darkGray);
+         hotelList.setText(lorem);
+         hotelList.setEditable(false);
+         hotelList.setWrapStyleWord(true);
+         hotelList.setLineWrap(true);
+         hotelList.setBounds(200, 200, 150, 100);
+         
+         //Border for JScrollPane
+         Border textBorder = BorderFactory.createLineBorder(Color.WHITE, 1);
+         
+         //JScrollPane for JTextArea
+         JScrollPane vertScroll = new JScrollPane(hotelList);
+         vertScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+         vertScroll.setBorder(textBorder);
+
+         //JLabel in JPanel
+         JLabel hotelListText = new JLabel("HIGH LEVEL INFORMATION");
+         hotelListText.setVerticalTextPosition(JLabel.TOP);
+         hotelListText.setHorizontalTextPosition(JLabel.CENTER);
+         hotelListText.setForeground(darkGray);
+         hotelListText.setFont(new Font("Helvetica", Font.BOLD, 13));
+
+         //JPanel for JLabel and JTextArea
+         RoundedPanel textHolder = new RoundedPanel();
+         textHolder.setBounds(200, 180, 300, 150);
+         textHolder.setBackground(darkBlue);
+         textHolder.add(hotelListText);
+         textHolder.add(vertScroll);
+
+         //Buttons
+        RoundedButton exButton = new RoundedButton("Exit");
+        exButton.setFont(new Font("Helvetica", Font.PLAIN, 14));
+        exButton.setForeground(Color.WHITE);
+        exButton.setBackground(Color.RED);
+        exButton.setBounds(170, 450, 100, 35); 
+        exButton.setOpaque(true); 
+        exButton.setBorder(null);
+        exButton.setContentAreaFilled(true); 
+        exButton.setCustomBorderColor(Color.WHITE); 
+        exButton.setCustomBorderThickness(2);
+        
+         //Button ActionListener
+         exButton.addActionListener(e -> {
+            ViewHotelPage mh = new ViewHotelPage();
+            mh.launchViewHotelPage(mainFrame);
+        });
+         
+         //Label for Menu label
+         JLabel menuLabel = new JLabel("HOTEL INFO");
+         menuLabel.setHorizontalTextPosition(JLabel.RIGHT);
+         menuLabel.setVerticalTextPosition(JLabel.TOP);
+         menuLabel.setForeground(Color.WHITE);
+         menuLabel.setFont(new Font("Helvetica", Font.BOLD, 32));
+         menuLabel.setBounds(250, -115, bgImage.getIconWidth(), bgImage.getIconHeight());
+ 
+         //Layers
+         layeredPane.add(menuLabel, JLayeredPane.PALETTE_LAYER); 
+         layeredPane.add(textHolder, JLayeredPane.POPUP_LAYER);
+         layeredPane.add(exButton, JLayeredPane.POPUP_LAYER);
+
+
+         //JFrame
+         mainFrame.setTitle("Hotel Reservation System");
+         mainFrame.setSize(660, 530);
+         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         mainFrame.setResizable(false);
+         mainFrame.setIconImage(logoImage.getImage());
+ 
+         //backgroundPanel and layeredPane on JFrame
+         mainFrame.setContentPane(backgroundPanel);
+         mainFrame.getContentPane().setLayout(new BorderLayout());
+         mainFrame.getContentPane().add(layeredPane, BorderLayout.CENTER);
+ 
+         mainFrame.setLocationRelativeTo(null); // Center JFrame on screen
+         mainFrame.setVisible(true);
+    }
+}
+
